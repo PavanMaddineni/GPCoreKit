e426c7fa3ebd335806a931c76baa60b9242582c5
Pavan Maddineni <maddineni.pavan@gmail.com>
2022-04-23 06:35:57 -0500

    Add files via upload

---
+++ SystemImages.swift
@@ -0,0 +1,14 @@
+//
+//  SystemImages.swift
+//  GPKit
+//
+//  Created by sn99 on 28/03/22.
+//
+
+import Foundation
+
+
+public enum SystemImages : String {
+    
+    case xmark_circle = "xmark.circle"
+}
