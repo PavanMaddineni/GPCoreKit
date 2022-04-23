ac3244084b37c3c6f8fdce67ab5e78a4b7221240
Pavan Maddineni <maddineni.pavan@gmail.com>
2022-04-23 06:27:45 -0500

    Initial commit

---
+++ Tests/GPCoreKitTests/GPCoreKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GPCoreKit
+
+final class GPCoreKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GPCoreKit().text, "Hello, World!")
+    }
+}
